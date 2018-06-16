c730264b00b2ca90290c84c4313ac27a1fe81614
Caitlin <caitlinsweeneylmt@gmail.com>
2018-06-16 15:09:19 -0700

    trouble shooting route

--- routes/routes.js
+++ routes/routes.js
@@ -18,26 +18,24 @@ const fs = require('fs');
 
 movieRouter.route('/movie-req').post((req,res) => {
   console.log('hi');
-  // superagent.post(`https://api.themoviedb.org/3/movie/550?api_key=${api_key}`)
-  //   .then(results => {
-  //     console.log(results);
-  //     Movie.create({
-  //       movie_id: results.body[id],
-  //     });
-  //   })
-  //   .then(results => {
-  
-  const responseBody = { headers, method, url, body };
-
-  response.write(JSON.stringify(responseBody));
-  response.end();
+  superagent.post(`https://api.themoviedb.org/3/movie/550?api_key=${api_key}`)
+    .then(results => {
+      console.log(results);
+      Movie.create({
+        movie_id: results.body[id],
+      });
+    });
 })
+  .then(results => {
+    res.status(200).send("GOAL",results);
+  })
   .catch(err => {
-    res.status(404).send("invalid response");
+    res.status(404).send("ERROR");
     console.log('error was thrown', err);
  
   });
-// });
+
+
   
 
 module.exports = movieRouter;
