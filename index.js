9f2887022fe48530dccc31377fb03580bad4f2f1
dambergn <dambergn@gmail.com>
2018-06-21 20:44:24 -0700

    trying to add server

--- index.js
+++ index.js
@@ -5,31 +5,44 @@ require('dotenv').config();
 // const mongoose = require('mongoose');
 // mongoose.connect(process.env.MONGODB_URI);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
+const api_key = process.env.api_key;
 const express = require('express');
 const bodyParser = require('body-parser');
+const superagent = require('superagent');
 const movieRouter = require('./routes/routes');
 const cors = require('cors');
-
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(cors());
+
+
+
 app.use('/api', movieRouter);
+
+// gets movies based on user search by title
+app.get('/api/movies/:title', (req, res) => {
+  let url_search = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${req.params.title}`;
+  superagent.get(url_search)
+    .then(data => {
+      console.log('api title:', req.params.title)
+      res.send(data.body.results);
+    })
+    .catch(err => console.error(err));
+});
 
 app.use(express.static('./dist'));
 
-app.use((req, res) => {
+app.use('/', (req, res) => {
   res.sendFile('index.html', { root: './dist' });
 });
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
 });
-
 
 // const server = module.exports = {};
 // server.isOn = false;
--- routes/routes.js
+++ routes/routes.js
@@ -10,62 +10,52 @@ const api_key = process.env.api_key;
 const movieRouter = new express.Router();
 const path = require('path');
 const fs = require('fs');
+const cors = require('cors');
 
-// movieRouter.route('/movie-req').post((req,res) => {
-//   console.log('hi');
-//   superagent.post(`https://api.themoviedb.org/3/movie/550?api_key=${api_key}`)
-//     .then(results => {
-//       console.log(results);
-//       Movie.create({
-//         movie_id: results.body[id],
-//       });
-//     });
-// })
-//   .then(results => {
-//     res.status(200).send("GOAL",results);
-//   })
-//   .catch(err => {
-//     res.status(404).send("ERROR");
-//     console.log('error was thrown', err);
- 
-// });
-movieRouter.route('/movie-req').get((req,res) => {
-  
-   console.log('hit router');
-   let msg = JSON.stringify('Sorry, we cannot find that');
-   res.send(msg);
+movieRouter.use(cors());
+
+movieRouter.route('/movie-req').get((req, res) => {
+let result = superagent.get(`https://api.themoviedb.org/3/movie/550?api_key=${api_key}`
+).end((err, result) => {
+  console.log('results from movie db', JSON.parse(result.text));
+  res.send(result.text);
+
 })
-//   let results = superagent.get(`https://api.themoviedb.org/3/movie/550?api_key=${api_key}`);
-//   return results; 
+
+// movieRouter.route('/movie-req').get((req, res) => {
+//   res.console.log('I have been hit1!')
+//   console.log('I have been hit2!').end();
+  
+//   });
+
+// gets movies based on user search by title
+// movieRouter.route('/movies/:title').get((req, res) => {
+//   console.log('I have been hit!');
+//   let url_search = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${req.params.title}`;
+//   superagent.get(url_search)
+//     .then(data => {
+//       console.log(req.params.title)
+//       res.send(data.body.results);
+//     })
+//     .catch(err => console.error(err));
+// });
+
+// .catch(err => {
+//     console.log('error was thrown', err);
+//     res.status(404).send('Sorry, we cannot find that!');
+//   })
 // })
 //   .then(results => {
 //     console.log(results);
 //         	return Movie.create({
 //       movie_id: results.body[id],
 //     });
-//   })
-//  .catch(err => {
-//   console.log('error was thrown', err);
-//   res.status(404).send('Sorry, we cannot find that!');
-//  });
+})
+
 
 module.exports = movieRouter;
 
-// old code that was throwing errors, brought in code from caitlin routes branch above 
-// movieRouter.route('/movie-req').post((req,res) => {
-//   console.log('hi');
-//   let results = superagent.post(`https://api.themoviedb.org/3/movie/550?api_key=${api_key}`);
-//   return results; 
+// console.log('hit router');
+// let msg = JSON.stringify('Sorry, we cannot find that');
+// res.send(msg);
 // })
-//   .then(results => {
-//     console.log(results);
-//     return Movie.create({
-//       movie_id: results.body[id],
-//     });
-//   })
-//   .catch(err => {
-//     console.log('error was thrown', err);
- 
-//   });
-
-// module.exports = movieRouter;
